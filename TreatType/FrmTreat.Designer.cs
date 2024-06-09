27d02cb2b43fb6d8ec5e130b39d493d0ad1b016b
Has Vuthy <149591543+HasVuthy@users.noreply.github.com>
2024-06-09 13:12:31 +0700

    Add files via upload

---
+++ TreatType/FrmTreat.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace TreatmentType.TreatType
+{
+    partial class FrmTreat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.TreatmentTypeID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TreatmentTypeName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(391, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TreatmentTypeID";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(391, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 43);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TreatmentTypeName";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(391, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 43);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TreatmentTypeDesc";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TreatmentTypeID,
+            this.TreatmentTypeName,
+            this.Description});
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dgv.Location = new System.Drawing.Point(0, 0);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(385, 450);
+            this.dgv.TabIndex = 3;
+            // 
+            // TreatmentTypeID
+            // 
+            this.TreatmentTypeID.DataPropertyName = "TreatmentTypeID";
+            this.TreatmentTypeID.HeaderText = "TreatmentTypeID";
+            this.TreatmentTypeID.Name = "TreatmentTypeID";
+            // 
+            // TreatmentTypeName
+            // 
+            this.TreatmentTypeName.DataPropertyName = "TreatmentTypeName";
+            this.TreatmentTypeName.HeaderText = "TreatmentTypeName";
+            this.TreatmentTypeName.Name = "TreatmentTypeName";
+            // 
+            // Description
+            // 
+            this.Description.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Description.DataPropertyName = "Description";
+            this.Description.HeaderText = "Description";
+            this.Description.Name = "Description";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(531, 21);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(223, 20);
+            this.txtID.TabIndex = 9;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(531, 78);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(223, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(531, 133);
+            this.txtDesc.Multiline = true;
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(223, 131);
+            this.txtDesc.TabIndex = 2;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(500, 333);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(77, 35);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(607, 333);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(77, 35);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(711, 333);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(77, 35);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(394, 333);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(77, 35);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // FrmTreat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmTreat";
+            this.Text = "FrmTreat";
+            this.Load += new System.EventHandler(this.FrmTreat_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TreatmentTypeID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TreatmentTypeName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+    }
+}
---
+++ TreatType/FrmTreat.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace TreatmentType.TreatType
+{
+    public partial class FrmTreat : Form
+    {
+        protected SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-HKMUMI1\DBS;Initial Catalog=ISAD;Integrated Security=True;");
+        protected DataSet dataSet = new DataSet();
+        protected SqlDataAdapter adapter = new SqlDataAdapter();
+        protected SqlCommand command = new SqlCommand();
+        protected BindingSource bindingSource = new BindingSource();
+        public static string TableTreat = "TreatmentTypeInfo";
+
+        public FrmTreat()
+        {
+            InitializeComponent();
+            setUp();
+        }
+
+        protected void setUp()
+        {
+            try
+            {
+                command.Connection = connection;
+                connection.Open();
+                command.CommandText = "SELECT ISNULL(MAX(TreatmentTypeID), 0) + 1 AS NewTreatID FROM TreatmentTypeInfo"; // Use the view
+                int newTreatID = Convert.ToInt32(command.ExecuteScalar());
+                txtID.Text = newTreatID.ToString();
+
+                command.CommandText = "SELECT * FROM TreatmentTypeInfo"; // Use the view
+                adapter.SelectCommand = command;
+                adapter.TableMappings.Add("Table", TableTreat);
+                adapter.Fill(dataSet);
+                bindingSource.DataSource = dataSet;
+                bindingSource.DataMember = TableTreat;
+                dgv.DataSource = bindingSource;
+
+                btnSubmit.Click += DoClickSubmit;
+                btnUpdate.Click += DoClickUpdate;
+                btnDelete.Click += DoClickDelete;
+                btnClear.Click +=DoClickClear;
+                dgv.CellClick += dgv_CellClick;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataRowView rowView = (DataRowView)dgv.Rows[e.RowIndex].DataBoundItem;
+                if (rowView != null)
+                {
+                    txtID.Text = rowView["TreatmentTypeID"].ToString();
+                    txtName.Text = rowView["TreatmentTypeName"].ToString();
+                    txtDesc.Text = rowView["Description"].ToString();
+                }
+            }
+        }
+
+        // Insert
+        private void DoClickSubmit(object sender, EventArgs e)
+        {
+            string treatmentTypeName = txtName.Text;
+            string description = txtDesc.Text;
+
+            // Basic validation (optional)
+            if (string.IsNullOrEmpty(treatmentTypeName) || string.IsNullOrEmpty(description))
+            {
+                MessageBox.Show("Please fill in all required fields.");
+                return;
+            }
+
+            try
+            {
+                InsertTreatmentType(treatmentTypeName, description);
+                MessageBox.Show("Record inserted successfully.");
+
+                // Clear textboxes after insert
+                txtName.Text = "";
+                txtDesc.Text = "";
+
+                // Refresh data grid
+                RefreshData();
+            }
+            catch (SqlException ex)
+            {
+                // Handle SQL specific errors
+                MessageBox.Show($"A database error occurred: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Handle general exceptions
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void RefreshData()
+        {
+            dataSet.Clear();
+            adapter.Fill(dataSet);
+        }
+
+        private void InsertTreatmentType(string TreatmentTypeName, string Description)
+        {
+            using (SqlCommand insertCommand = new SqlCommand("ProInsertTreatmentType", connection))
+            {
+                insertCommand.CommandType = CommandType.StoredProcedure;
+
+                insertCommand.Parameters.AddWithValue("@TreatmentTypeName", TreatmentTypeName);
+                insertCommand.Parameters.AddWithValue("@Description", Description);
+
+                SqlParameter outputParam = new SqlParameter("@TreatmentTypeID", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                insertCommand.Parameters.Add(outputParam);
+
+                try
+                {
+                    if (connection.State != ConnectionState.Open)
+                    {
+                        connection.Open();
+                    }
+                    insertCommand.ExecuteNonQuery(); // Execute the stored procedure
+                    int newTreatmentTypeID = Convert.ToInt32(outputParam.Value);
+                    txtID.Text = newTreatmentTypeID.ToString();
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void DoClickUpdate(object sender, EventArgs e)
+        {
+            // Get the values from the text boxes
+            string newTreatmentTypeName = txtName.Text;
+            string newDescription = txtDesc.Text;
+
+            // Get the index of the selected row in the DataGridView
+            int selectedIndex = dgv.CurrentCell.RowIndex;
+
+            // Check if a row is selected
+            if (selectedIndex >= 0)
+            {
+                // Get the DataRow associated with the selected row
+                DataRowView rowView = (DataRowView)dgv.Rows[selectedIndex].DataBoundItem;
+                DataRow row = rowView.Row;
+
+                // Get the TreatmentTypeID from the selected row
+                int treatmentTypeID = Convert.ToInt32(row["TreatmentTypeID"]);
+
+                // Update the database
+                try
+                {
+                    UpdateTreatmentType(treatmentTypeID, newTreatmentTypeName, newDescription);
+                    MessageBox.Show("Record updated successfully.");
+
+                    // Refresh data grid
+                    RefreshData();
+                }
+                catch (SqlException ex)
+                {
+                    // Handle SQL specific errors
+                    MessageBox.Show($"A database error occurred: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    // Handle general exceptions
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to update.");
+            }
+        }
+
+        private void UpdateTreatmentType(int treatmentTypeID, string newTreatmentTypeName, string newDescription)
+        {
+            // Open connection
+            using (SqlConnection conn = new SqlConnection(connection.ConnectionString))
+            {
+                conn.Open();
+
+                // Create command
+                using (SqlCommand cmd = new SqlCommand("UpdateTreatmentType", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // Add parameters
+                    cmd.Parameters.AddWithValue("@TreatmentTypeID", treatmentTypeID);
+                    cmd.Parameters.AddWithValue("@TreatmentTypeName", newTreatmentTypeName);
+                    cmd.Parameters.AddWithValue("@Description", newDescription);
+
+                    // Execute command
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void DoClickDelete(object sender, EventArgs e)
+        {
+            // Get the index of the selected row in the DataGridView
+            int selectedIndex = dgv.CurrentCell.RowIndex;
+
+            // Check if a row is selected
+            if (selectedIndex >= 0)
+            {
+                // Get the DataRow associated with the selected row
+                DataRowView rowView = (DataRowView)dgv.Rows[selectedIndex].DataBoundItem;
+                DataRow row = rowView.Row;
+
+                // Get the TreatmentTypeID from the selected row
+                int treatmentTypeID = Convert.ToInt32(row["TreatmentTypeID"]);
+
+                // Delete the record from the database
+                try
+                {
+                    DeleteTreatmentType(treatmentTypeID);
+                    MessageBox.Show("Record deleted successfully.");
+
+                    // Refresh data grid
+                    RefreshData();
+                }
+                catch (SqlException ex)
+                {
+                    // Handle SQL specific errors
+                    MessageBox.Show($"A database error occurred: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    // Handle general exceptions
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete.");
+            }
+        }
+
+        private void DeleteTreatmentType(int treatmentTypeID)
+        {
+            // Open connection
+            using (SqlConnection conn = new SqlConnection(connection.ConnectionString))
+            {
+                conn.Open();
+
+                // Create command
+                using (SqlCommand cmd = new SqlCommand("ProDeleteTreatmentType", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // Add parameter
+                    cmd.Parameters.AddWithValue("@TreatmentTypeID", treatmentTypeID);
+
+                    // Execute command
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void DoClickClear(object sender, EventArgs e)
+        {
+            // Clear textboxes
+            txtID.Text = "";
+            txtName.Text = "";
+            txtDesc.Text = "";
+
+            // Optionally, set focus to the first textbox
+            txtName.Focus();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void FrmTreat_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
